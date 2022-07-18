4e4c33bb6442408e1c6112a11528f85ae66ddf17
PierreDoumenc <pierre.doumenc@lunatech.nl>
2022-07-18 11:12:51 +0200

    changed base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/newdeal/',
+  base: '/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
